6a807cb3178a757e32d4fa58b4bce96ad1806350
MmGrand <maksimzarubin20002000@gmail.com>
2024-07-18 21:18:54 +0700

    tabs_ended

--- js/main.js
+++ js/main.js
@@ -48,3 +48,30 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.slider__btn--prev',
   },
 });
+
+// Tabs
+const tabsBtns = document.querySelectorAll('[data-tab]');
+const tabsProducts = document.querySelectorAll('[data-tab-value]');
+
+for(let btn of tabsBtns) {
+	btn.addEventListener('click', function () {
+
+		for(let btn of tabsBtns) {
+			btn.classList.remove('tab-controls__btn--active');
+		}
+
+		this.classList.add('tab-controls__btn--active');
+
+		for(let product of tabsProducts) {
+			if(this.dataset.tab === 'all') {
+				product.classList.remove('none');
+			} else if(product.dataset.tabValue === this.dataset.tab) {
+				product.classList.remove('none');
+			} else {
+				product.classList.add('none');
+			}
+		}
+
+		swiper.update();
+	});
+}
